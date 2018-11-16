c7f3c6398854377faf42b8afe915512c47493f05
noaEylon <37585299+NoaAylon@users.noreply.github.com>
2018-11-16 19:23:41 +0200

    calculator Final version

--- client_app/script.js
+++ client_app/script.js
@@ -1,53 +1,42 @@
 $(document).ready(function () {
-
-
     $("#answer").click(function () {
-        console.log('clicked!!!');
-
         let num1 = parseInt($('input:text')[0].value);
         let num2 = parseInt($('input:text')[1].value);
         let num3 = parseInt($('input:text')[2].value);
-
         let method = $('input[name=method]:checked').val();
-        let chosen_func = $('input[name=function]:checked').val();
-
-        console.log(num1);
-        console.log(num2);
-        console.log(num3);
-        console.log(method);
-        console.log(chosen_func);
+        let func = $('input[name=function]:checked').val();
 
         if (method == 'GET') {
             console.log("inside get");
             $.ajax({
-                url: `../service_calculator/index.php?num1=${num1}&num2=${num2}&num3=${num3}&func=${chosen_func}`,
+                url: `/calculator/service_calculator/index.php?num1=${num1}&num2=${num2}&num3=${num3}&func=${func}`,
                 success: function (data) {
                     $(".result").text(`result: ${data.result}`);
                     console.log("Return data: " + data.result);
+                },
+                error: function (error) {
+                    console.log(error);
                 }
-
-
             });
-
         } else {
             $.ajax({
-                url: `../service_calculator/index.php`,
+                url: `/calculator/service_calculator/index.php`,
                 type: method,
                 data: {
-                    num1: num1,
-                    num2: num2,
-                    num3: num3,
-                    func: chosen_func
+                    num1,
+                    num2,
+                    num3,
+                    func
                 },
                 success: function (data) {
                     $(".result").text(`result: ${data.result}`);
                     console.log("Return data: " + data.result);
+                },
+                error: function (error) {
+                    console.log(error);
+                    $(".result").text(`error: Please choose function`).css('color', 'red');
                 }
             });
-
         }
-
-
     });
-
 });
--- service_calculator/index.php
+++ service_calculator/index.php
@@ -1,26 +1,54 @@
 <?php
 
+    $method = $_SERVER['REQUEST_METHOD'];
+
+    if ($method == "PUT"){
+        parse_str(file_get_contents("php://input"),$_PUT);
+        $func = $_PUT['func'];
+    }
+    
     if(isset($_GET['num1'])) // receive data via GET
         $num1 = (int)$_GET["num1"]; // convert String to Integer
     else if(isset($_POST['num1'])) // receive data via POST
         $num1 = (int)$_POST["num1"]; // convert String to Integer
     else if(isset($_PUT['num1'])) // receive data via PUT
         $num1 = (int)$_PUT["num1"]; // convert String to Integer
-    else $num1 = 50; // if not set=>default will be 50. BTW, if not set, it will be 0.
+    else $num1 = 0; // if not set=>default will be 50. BTW, if not set, it will be 0.
 
     if(isset($_GET['num2'])) $num2 = (int)$_GET["num2"];
     else if(isset($_POST['num2'])) $num2 = (int)$_POST["num2"];
     else if(isset($_PUT['num2'])) $num2 = (int)$_PUT["num2"];
-    else $num2 = 50;
+    else $num2 = 0;
 
     if(isset($_GET['num3'])) $num3 = (int)$_GET["num3"];
     else if(isset($_POST['num3'])) $num3 = (int)$_POST["num3"];
     else if(isset($_PUT['num3'])) $num3 = (int)$_PUT["num3"];
-    else $num3 = 50;
+    else $num3 = 0;
 
     if(isset($_GET['func'])) $func = $_GET['func'];
     else if (isset($_POST['func'])) $func = $_POST['func'];
     else $func = $_PUT['func'];
+
+   
+
+    $calculator = new Calc();
+
+    if($func == 'sum'){
+       $res = $calculator->sum($num1,$num2,$num3);
+    }else if($func == 'mult'){
+      $res = $calculator->mult($num1,$num2,$num3);
+    }else if($func == 'avg'){
+       $res = $calculator->avg($num1,$num2,$num3);
+    }else{
+        header("HTTP/1.1 404 Not Found");
+        return print('Error');
+    }
+
+    $a = array('result'=>$res); // build the results Array
+
+    header('Content-Type: application/json'); // set header for json response
+    echo json_encode($a); // echo the converted JSON Object from the Array
+    return;
 
     class Calc{
         
@@ -45,22 +73,6 @@
         }
     }
 
-    $calculator = new Calc();
-
-    if($func == 'sum'){
-       $res = $calculator->sum($num1,$num2,$num3);
-    }else if($func == 'mult'){
-      $res = $calculator->mult($num1,$num2,$num3);
-    }else if($func == 'avg'){
-       $res = $calculator->avg($num1,$num2,$num3);
-    }else{
-        return print('Error');
-    }
-
-    $a = array('result'=>$res); // build the results Array
-
-    header('Content-Type: application/json'); // set header for json response
-    echo json_encode($a); // echo the converted JSON Object from the Array
 ?>
 
 
